e7025c608f2c28ec8f28d42b2618fa6b5e530551
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-08-17 11:58:20 -0300

    Create CartContext.tsx

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,97 @@
+import { createContext, ReactNode, useEffect, useState } from "react"
+import { produce } from "immer"
+import { Food } from "../components/OurFood/CardFoods";
+
+export interface CartItem extends Food {
+  quantity: number;
+}
+
+interface CartContextType {
+  cartItems: CartItem[]
+  cartQuantity: number
+  cartItemsTotal: number
+  addFoodToCart: (food: CartItem) => void;
+  changeCartItemQuantity: (
+    cartItemId: number,
+    type: "increase" | "decrease"
+  ) => void;
+  removeCartItem: (cartItemId: number) => void;
+}
+
+export const CartContext = createContext({} as CartContextType);
+
+interface CartContextProviderProps {
+  children: ReactNode;
+}
+
+export function CartContextProvider({ children }: CartContextProviderProps){
+  const [cartItems, setCartItems] = useState([])
+
+  const cartQuantity = cartItems.length;
+
+  const cartItemsTotal = cartItems.reduce((total, cartItem) => {
+    return total + cartItem.price * cartItem.quantity;
+  }, 0);
+
+  function addFoodToCart(food: CartItem) {
+    const foodAlreadyExistsInCart = cartItems.findIndex(
+      (cartItem) => cartItem.id === food.id
+    );
+
+    const newCart = produce(cartItems, (draft) => {
+      if (foodAlreadyExistsInCart < 0) {
+        draft.push(food);
+      } else {
+        draft[foodAlreadyExistsInCart].quantity += food.quantity;
+      }
+    });
+    setCartItems(newCart);
+  }
+
+  function changeCartItemQuantity(
+    cartItemId: number,
+    type: "increase" | "decrease"
+  ) {
+    const newCart = produce(cartItems, (draft) => {
+      const foodExistsInCart = cartItems.findIndex(
+        (cartItem) => cartItem.id === cartItemId
+      );
+
+      if (foodExistsInCart >= 0) {
+        const item = draft[foodExistsInCart];
+        draft[foodExistsInCart].quantity =
+          type === "increase" ? item.quantity + 1 : item.quantity - 1;
+      }
+    });
+    setCartItems(newCart);
+  }
+
+  function removeCartItem(cartItemId: number) {
+    const newCart = produce(cartItems, (draft) => {
+      const foodExistsInCart = cartItems.findIndex(
+        (cartItem) => cartItem.id === cartItemId
+      );
+
+      if (foodExistsInCart >= 0) {
+        draft.splice(foodExistsInCart, 1);
+      }
+    });
+
+    setCartItems(newCart);
+  }
+
+  
+  return(
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addFoodToCart,
+        cartQuantity,
+        changeCartItemQuantity,
+        removeCartItem,
+        cartItemsTotal,
+      }}>
+      {children}
+    </CartContext.Provider>
+   )
+}
