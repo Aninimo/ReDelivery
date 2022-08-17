5bf58956027a3cc8ec142d0333ce48e4fce3c51b
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-08-17 12:51:12 -0300

    Create foods.ts

---
+++ src/data/foods.ts
@@ -0,0 +1,36 @@
+export const foods = [
+  {
+    id: 1,
+    name: 'Pizza',
+    photo: 'https://pngimg.com/uploads/pizza/small/pizza_PNG44049.png',
+    price: 13.40
+  },
+
+  {
+    id: 2,
+    name: 'Hamburguer',
+    photo: 'https://pngimg.com/uploads/burger_sandwich/small/burger_sandwich_PNG96787.png',
+    price: 10.70
+  },
+
+  {
+    id: 3,
+    name: 'Potato chips',
+    photo: 'https://pngimg.com/uploads/potato_chips/small/potato_chips_PNG82.png',
+    price: 8.90
+  },
+
+  {
+    id: 4,
+    name: 'Pepper pizza',
+    photo: 'https://pngimg.com/uploads/pizza/small/pizza_PNG44088.png',
+    price: 12.75
+  },
+
+  {
+    id: 5,
+    name: 'Fries',
+    photo:'https://pngimg.com/uploads/fries/small/fries_PNG16.png',
+    price: 13.85
+  }
+]
